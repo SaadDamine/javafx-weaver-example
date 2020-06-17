6adb311303aa29d915734eaff5f075bc57e6b0cb
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-17 17:54:48 +0100

    create empty LoginController with fxml file

--- src/main/java/com/damine/javafxweaverexample/JavaFxApplication.java
+++ src/main/java/com/damine/javafxweaverexample/JavaFxApplication.java
@@ -1,5 +1,6 @@
 package com.damine.javafxweaverexample;
 
+import com.damine.javafxweaverexample.controllers.LoginController;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
@@ -25,7 +26,7 @@ public class JavaFxApplication extends Application {
     @Override
     public void start(Stage stage) {
         FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
-        Parent root = fxWeaver.loadView(MyController.class);
+        Parent root = fxWeaver.loadView(LoginController.class);
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
